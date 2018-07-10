caa1164b1eb3f3f366b3710eb41c9cf9092596f5
Marcus <marcus2cu@gmail.com>
2018-07-10 15:36:53 +0100

    refactor quotes script to start onload

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,25 +1,32 @@
-let introText = () =>{
+let quotes = [
+    {
+        quote: `The palest ink is better than the best memory. <br><br> ~~ "<em>Chinese Proverb</em>"`
+    },
+    {
+        quote: `Own only what you can carry with you; know language, know countries, know people. 
+        Let your memory be your travel bag. <br><br> ~~ "<em>Alexandr Solzhenitsyn</em>"`
+    },
+    {
+        quote: `The more man meditates upon good thoughts, 
+        the better will be his world and the world at large.<br><br> ~~ "<em>Confucius</em>"`
+    }
+];
 
-    let quotes = [
-        {
-            quote: `The palest ink is better than the best memory. <br><br> ~~ "<em>Chinese Proverb</em>"`
-        },
-        {
-            quote: `Own only what you can carry with you; know language, know countries, know people. 
-            Let your memory be your travel bag. <br><br> ~~ "<em>Alexandr Solzhenitsyn</em>"`
-        },
-        {
-            quote: `The more man meditates upon good thoughts, 
-            the better will be his world and the world at large.<br><br> ~~ "<em>Confucius</em>"`
-        }
-    ];
-
+let winload = () =>{
     quotes.map((v, i, arr) => {
-        return setInterval(function(){
+        document.querySelector('#quotes').innerHTML = arr[Math.floor(Math.random()*arr.length)].quote;
+    });
+}
+let introQuote = () =>{
+    quotes.map((v, i, arr) => {
+        setInterval(function(){
             document.querySelector('#quotes').innerHTML = arr[Math.floor(Math.random()*arr.length)].quote;
         }, 2000)
     });
 }
 
-window.onload = introText();
+window.onload = () => {
+    winload();
+    introQuote();
+}
 
