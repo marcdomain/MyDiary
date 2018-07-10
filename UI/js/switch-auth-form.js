1def66367c433b415482a0995811d4bb95f99fe8
Marcus <marcus2cu@gmail.com>
2018-07-10 22:36:07 +0100

    create switch-auth-form script

---
+++ UI/js/switch-auth-form.js
@@ -0,0 +1,21 @@
+
+const userForms = () => {
+    const authForm = document.querySelectorAll(".auth-form");
+    authForm[0].style.display = 'block';
+
+    function switchForm(event){
+        if(event.target.id === "signin"){
+            authForm[0].style.display = "none";
+            authForm[1].style.display = "block";
+        }
+        else {
+            authForm[1].style.display = "none";
+            authForm[0].style.display = "block";
+        }
+    }
+    const signup = document.querySelector("#signup");
+    const signin = document.querySelector("#signin");
+    signup.addEventListener("click", switchForm);
+    signin.addEventListener("click", switchForm);
+};
+window.onload = userForms();
