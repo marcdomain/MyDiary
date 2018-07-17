80cedfacd7e244e75900f7de639dc54416edc313
Marcus <marcus2cu@gmail.com>
2018-07-17 12:12:19 +0100

    setup nodemon and create start script

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const port = process.env.PORT || 3310;
+app.listen(port, () => {
+  console.log('server listening on port', port);
+});
