89904f566206dde86f0eff3ebf75bea52417831f
Marcus <marcus2cu@gmail.com>
2018-07-17 19:51:53 +0100

    create get a diary entry controller

--- server/controllers/entriesController.js
+++ server/controllers/entriesController.js
@@ -9,6 +9,23 @@ class DiaryEntriesHandler {
         message: 'All diary entries served'
       });
   }
+
+  static getADiaryEntry(req,res) {
+      const { entryId } = req.params;
+      const foundEntry = entries.find(entries => entries.id === parseInt(entryId, 10));
+      if (!foundEntry) {
+        return res.status(404)
+          .json({
+            message: 'Entry not found',
+          });
+      }
+      return res.status(200)
+        .json({
+          entries: foundEntry,
+          message: 'Entry fetched successfully',
+        });
+  }
+
 }
 
 export default DiaryEntriesHandler;
