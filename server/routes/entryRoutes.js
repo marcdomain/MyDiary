273a450eeb25f8e536448b7379de5cbaae4b1e79
Marcus <marcus2cu@gmail.com>
2018-07-18 00:42:00 +0100

    create add-new-entry router

--- server/routes/entryRoutes.js
+++ server/routes/entryRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import entriesController from '../controllers/entriesController';
 
-const { getAllEntries, getADiaryEntry } = entriesController;
+const { getAllEntries, getADiaryEntry, postEntry } = entriesController;
 
 const router = express.Router();
 
 router.get("/entries", getAllEntries);
 router.get("/entries/:entryId", getADiaryEntry);
+router.post("/entries", postEntry);
 
 export default router;
