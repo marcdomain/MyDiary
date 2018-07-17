740e45316a333d33d390cb9a91df70a118a19ccf
Marcus <marcus2cu@gmail.com>
2018-07-17 19:59:08 +0100

    create router for get a diary entry

--- server/routes/entryRoutes.js
+++ server/routes/entryRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import entriesController from '../controllers/entriesController';
 
-const { getAllEntries } = entriesController;
+const { getAllEntries, getADiaryEntry } = entriesController;
 
 const router = express.Router();
 
 router.get("/entries", getAllEntries);
+router.get("/entries/:Id", getADiaryEntry);
 
 export default router;
