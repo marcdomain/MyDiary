faafcc1f48ff35771ced0fa26660d385d76519b4
Marcus <marcus2cu@gmail.com>
2018-07-18 23:10:00 +0100

    Create route for homepage and catch-all non-existing routes

--- server/routes/entryRoutes.js
+++ server/routes/entryRoutes.js
@@ -18,4 +18,14 @@ router.put("/entries/:entryId", modifyEntryValidator, modifyEntry);
 router.post("/users/signup", signupValidator, userSignUp);
 router.post("/users/signin", signinValidator, userSignin);
 
+router.get('/', (req, res) => res.status(200)
+  .json({
+      message: 'Welcome to Marcodynamics MyDiary App! Create an account and start penning down your Thoughts/Feelings.'
+  }));
+
+router.all('*', (req, res) => res.status(404)
+  .json({
+    message: 'Oops! 404. Page not Found',
+  }));
+
 export default router;
