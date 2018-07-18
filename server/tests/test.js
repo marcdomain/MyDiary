358094ea35aed01ec54b2635ac9a7e4bbe4fe76a
Marcus <marcus2cu@gmail.com>
2018-07-19 00:49:46 +0100

    install test-reporting dependencies and test default API

--- server/tests/test.js
+++ server/tests/test.js
@@ -5,3 +5,34 @@ import chaiHttp from 'chai-http';
 const { expect } = chai;
 
 chai.use(chaiHttp);
+
+describe('Test Default API Endpoints', () => {
+  it('Should return 200 for homepage', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equal('Welcome to Marcodynamics MyDiary App! Create an account and start penning down your Thoughts/Feelings.');
+        done();
+      });
+  });
+  
+  it('Should return 404 for routes not specified', (done) => {
+    chai.request(app)
+      .get('/another/undefined/route')
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        done();
+      });
+  });
+  it('Undefined Routes Should Return 404', (done) => {
+    chai.request(app)
+      .post('/another/undefined/route')
+      .send({ random: 'random' })
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        done();
+      });
+  });
+  
+})
