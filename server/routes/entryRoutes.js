b4e758930c52317c9ab3d184b6883a9dd891002b
Marcus <marcus2cu@gmail.com>
2018-07-18 02:07:14 +0100

    update user signup route

--- server/routes/entryRoutes.js
+++ server/routes/entryRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import entriesController from '../controllers/entriesController';
+import usersController from '../controllers/usersControlller';
 
 const { getAllEntries, getADiaryEntry, postEntry, modifyEntry } = entriesController;
+const { userSignUp } = usersController;
 
 const router = express.Router();
 
@@ -9,5 +11,6 @@ router.get("/entries", getAllEntries);
 router.get("/entries/:entryId", getADiaryEntry);
 router.post("/entries", postEntry);
 router.put("/entries/:entryId", modifyEntry);
+router.post("/users", userSignUp);
 
 export default router;
