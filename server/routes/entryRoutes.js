192fae3b762876b7c52bf87c88ce42bdb4e5ec59
Marcus <marcus2cu@gmail.com>
2018-07-18 03:33:27 +0100

    insert validator middleware into get-a-diary route

--- server/routes/entryRoutes.js
+++ server/routes/entryRoutes.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import entriesController from '../controllers/entriesController';
 import usersController from '../controllers/usersController';
+import entriesValidator from '../middlewares/entriesValidator';
 
 const { getAllEntries, getADiaryEntry, postEntry, modifyEntry } = entriesController;
 const { userSignUp, userSignin } = usersController;
+const { getADiaryEntryValidator } = entriesValidator;
 
 const router = express.Router();
 
 router.get("/entries", getAllEntries);
-router.get("/entries/:entryId", getADiaryEntry);
+router.get("/entries/:entryId", getADiaryEntryValidator, getADiaryEntry);
 router.post("/entries", postEntry);
 router.put("/entries/:entryId", modifyEntry);
 router.post("/users/signup", userSignUp);
