ec9d47247373149d561864ebf7ab0c671e32a1ae
Marcus <marcus2cu@gmail.com>
2018-07-19 21:18:43 +0100

    Adjust linting errors

--- server/dummyModels/entries.js
+++ server/dummyModels/entries.js
@@ -1,17 +1,16 @@
 export default [
-    {
-      id: 1,
-      username: 'marcodynamics',
-      email: 'marcus2cu@gmail.com',
-      title: 'My coding journey',
-      description: 'Coding has been an awesome experience so far...'
-    },
-    {
-        id: 2,
-        username: 'Marc1',
-        email: 'marc1@yahoo.com',
-        title: 'My reflection on Andela EPIC value',
-        description: 'The Andela values are carefully chosen for delivery of excellence in all dimensions...',
-    }
-  ];
-  
+  {
+    id: 1,
+    username: 'marcodynamics',
+    email: 'marcus2cu@gmail.com',
+    title: 'My coding journey',
+    description: 'Coding has been an awesome experience so far...'
+  },
+  {
+    id: 2,
+    username: 'Marc1',
+    email: 'marc1@yahoo.com',
+    title: 'My reflection on Andela EPIC value',
+    description: 'The Andela values are carefully chosen for delivery of excellence in all dimensions...',
+  }
+];
--- server/routes/entryRoutes.js
+++ server/routes/entryRoutes.js
@@ -2,25 +2,27 @@ import express from 'express';
 import entriesController from '../controllers/entriesController';
 import usersController from '../controllers/usersController';
 import entriesValidator from '../middlewares/entriesValidator';
-import userAuthValidator from '../middlewares/userAuthValidator'
+import userAuthValidator from '../middlewares/userAuthValidator';
 
-const { getAllEntries, getADiaryEntry, postEntry, modifyEntry } = entriesController;
+const {
+  getAllEntries, getADiaryEntry, postEntry, modifyEntry
+} = entriesController;
 const { userSignUp, userSignin } = usersController;
 const { getADiaryEntryValidator, modifyEntryValidator, postEntryValidator } = entriesValidator;
 const { signupValidator, signinValidator } = userAuthValidator;
 
 const router = express.Router();
 
-router.get("/entries", getAllEntries);
-router.get("/entries/:entryId", getADiaryEntryValidator, getADiaryEntry);
-router.post("/entries", postEntryValidator, postEntry);
-router.put("/entries/:entryId", modifyEntryValidator, modifyEntry);
-router.post("/users/signup", signupValidator, userSignUp);
-router.post("/users/signin", signinValidator, userSignin);
+router.get('/entries', getAllEntries);
+router.get('/entries/:entryId', getADiaryEntryValidator, getADiaryEntry);
+router.post('/entries', postEntryValidator, postEntry);
+router.put('/entries/:entryId', modifyEntryValidator, modifyEntry);
+router.post('/users/signup', signupValidator, userSignUp);
+router.post('/users/signin', signinValidator, userSignin);
 
 router.get('/', (req, res) => res.status(200)
   .json({
-      message: 'Welcome to Marcodynamics MyDiary App! Create an account and start penning down your Thoughts/Feelings.'
+    message: 'Welcome to Marcodynamics MyDiary App! Create an account and start penning down your Thoughts/Feelings.'
   }));
 
 router.all('*', (req, res) => res.status(404)
