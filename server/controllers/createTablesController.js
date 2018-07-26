7e42a093150dc6468f0f2cafd8a0f695310071a0
Marcus <marcus2cu@gmail.com>
2018-07-26 01:23:00 +0100

    add jsdoc to create-table-controller

--- server/controllers/createTablesController.js
+++ server/controllers/createTablesController.js
@@ -1,7 +1,22 @@
 import pool from './connect';
 import table from './dbTables';
 
+/*
+ * Class representing User Database Tables Handler
+ *
+ * @class TablesHandler
+ */
+
 class TablesHandler {
+  /*
+   * Reset or get a database table
+   *
+   * @static
+   * @param {object} req - The request object
+   * @param {object} res - The response object
+   * @returns {object} JSON object representing the response message
+   * @memberof TablesHandler
+   */
   static createTable(req, res) {
     pool.query(table.entriesTable)
       .then(result => res.status(201)
