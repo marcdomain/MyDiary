e96b44ca08ba91dc4d917fedecd25a008a5cc590
Marcus <marcus2cu@gmail.com>
2018-07-27 08:42:24 +0100

    remove unnecessary comments

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './server/routes/index';
-// import tables from './server/controllers/db/createTablesController';
-
-// const { createTable } = tables;
 
 const { defaultRouter, userRouter, entriesRouter } = router;
 
@@ -14,7 +11,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/v1', userRouter);
 app.use('/api/v1', entriesRouter);
-// app.use('/', createTable);
 app.use('/', defaultRouter);
 
 const port = process.env.PORT || 3310;
