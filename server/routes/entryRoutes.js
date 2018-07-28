dbca060ce702b1207b7c12c1c28111178c105b0c
Marcus <marcus2cu@gmail.com>
2018-07-28 08:17:05 +0100

    substitute verify.js with authenticator.js

--- server/routes/entryRoutes.js
+++ server/routes/entryRoutes.js
@@ -13,8 +13,8 @@ const { modifyEntryValidator } = entriesValidator;
 const entriesRouter = express.Router();
 
 entriesRouter.get('/entries', verifyToken, getAllEntries);
-// entriesRouter.get('/entries/:entryId', verifyToken, getADiaryEntry);
-// entriesRouter.post('/entries', verifyToken, postEntry);
-// entriesRouter.put('/entries/:entryId', modifyEntryValidator, modifyEntry);
+entriesRouter.get('/entries/:entryId', verifyToken, getADiaryEntry);
+entriesRouter.post('/entries', verifyToken, postEntry);
+entriesRouter.put('/entries/:entryId', modifyEntryValidator, modifyEntry);
 
 export default entriesRouter;
