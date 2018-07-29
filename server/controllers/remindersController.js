a78b27afbdbdff3673f815a8bd27f565cc96b892
Marcus <marcus2cu@gmail.com>
2018-07-29 16:27:48 +0100

    Create set-reminder controller action

--- server/controllers/remindersController.js
+++ server/controllers/remindersController.js
@@ -18,8 +18,27 @@ class ReminderSettingsHandler {
    * @param {object} res - The response object
    * @memberof ReminderSettingsHandler
    */
-  static setReminder(req, res){
+  static setReminder(req, res) {
+    req.body.username = req.authData.authUser[0].username;
+    req.body.email = req.authData.authUser[0].email;
+    const params = [
+      req.body.username,
+      req.body.email,
+      req.body.title,
+      req.body.date
+    ];
 
+    pool.query(insertIntoReminders, params)
+      .then(() => res.status(201)
+        .json({
+          message: 'Reminder recorded successfully!',
+        }))
+      .catch((err) => {
+        res.status(500)
+          .json({
+            message: err.message
+          });
+      });
   }
 }
 
