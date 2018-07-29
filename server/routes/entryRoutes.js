2b28eb49ed4d0c454d18f74da13e06918efd9a90
Marcus <marcus2cu@gmail.com>
2018-07-30 00:24:42 +0100

    update validations for diary-entries

--- server/routes/entryRoutes.js
+++ server/routes/entryRoutes.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import entriesController from '../controllers/entriesController';
 import verify from '../middlewares/authenticator';
+import validation from '../middlewares/entriesValidator';
+
+const { postEntryValidator, modifyEntryValidator } = validation;
 
 const { verifyToken } = verify;
 
@@ -12,8 +15,8 @@ const entriesRouter = express.Router();
 
 entriesRouter.get('/entries', verifyToken, getAllEntries);
 entriesRouter.get('/entries/:entryId', verifyToken, getADiaryEntry);
-entriesRouter.post('/entries', verifyToken, postEntry);
-entriesRouter.put('/entries/:entryId', verifyToken, modifyEntry);
+entriesRouter.post('/entries', postEntryValidator, verifyToken, postEntry);
+entriesRouter.put('/entries/:entryId', modifyEntryValidator, verifyToken, modifyEntry);
 entriesRouter.delete('/entries/:entryId', verifyToken, deleteEntry);
 
 export default entriesRouter;
