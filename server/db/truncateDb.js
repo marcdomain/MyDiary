096a8fcc6415cdae382d04673f7b52beca2c2228
Marcus <marcus2cu@gmail.com>
2018-08-02 14:52:17 +0100

    delete redundant database query

--- server/db/truncateDb.js
+++
@@ -1,7 +0,0 @@
-import pool from './connectDb';
-/* eslint-disable no-console */
-
-const truncateUsers = 'truncate users';
-pool.query(truncateUsers)
-  .then(() => (console.log('TRUNCATED users')))
-  .catch(err => console.log(`from database ${err}`));
